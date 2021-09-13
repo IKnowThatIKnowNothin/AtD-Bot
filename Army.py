7ee4b213fdcf3aa065818f499b2e4372916e83f3
IKnowThatIKnowNothin <35470181+IKnowThatIKnowNothin@users.noreply.github.com>
2021-09-13 21:17:44 +0100

    Create Army.py

---
+++ Army.py
@@ -0,0 +1,24 @@
+import re
+import random
+class Army:
+
+        commanderName = ""
+        commanderBonus = 0
+        name = ""
+        power = 0
+        bonus = 0
+        continueFighting = True
+        morale = 100
+        def __init__(self,commanderName,commanderBonus,name,power,bonus):
+                if (commanderName == "None"):
+                        self.commanderName = "The {} Commander".format(name)
+                else:
+                        self.commanderName = commanderName
+                        
+                self.commanderBonus = commanderBonus
+                self.name = name
+                self.power = power
+                self.armyBonus = bonus
+                self.bonus = self.armyBonus + self.commanderBonus
+        def attack_roll(self):
+                return random.randint(1,100) +self.bonus
