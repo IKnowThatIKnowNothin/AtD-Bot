532e7bc89f3ab3557acb2cf4fe333494a5025ffb
IKnowThatIKnowNothin <35470181+IKnowThatIKnowNothin@users.noreply.github.com>
2021-09-13 21:44:51 +0100

    Create Globals.py

---
+++ Globals.py
@@ -0,0 +1,8 @@
+import re
+class Globals:
+        
+        def run():
+                global battleType
+                battleType = "Land"
+                global resultsMode
+                resultsMode = False
