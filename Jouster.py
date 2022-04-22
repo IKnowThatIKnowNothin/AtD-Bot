c2eb2aeeeea74a5ad773835053d8cfe369cf0fda
IKnowThatIKnowNothin <35470181+IKnowThatIKnowNothin@users.noreply.github.com>
2022-04-22 05:33:33 +0100

    Create Jouster.py

---
+++ Jouster.py
@@ -0,0 +1,62 @@
+import re
+import random
+class Jouster:
+        name = ""
+        bonus = 0
+        injuryRoll = 0
+        continueFighting = True
+        ableToFight = True
+        brokenLances = 0
+        minorInjuries = 0
+        moderateInjuries = 0
+        majorInjuries = 0
+        permanentInjuries = "None"
+        alive = True
+        def __init__(self,name,bonus):
+                self.name = name
+                self.bonus = bonus
+        def attack_roll(self):
+                return random.randint(1,20) + self.bonus
+        def modify_bonus(self,mod):
+                self.bonus += mod
+        def injury_roll(self):
+                injuryRoll = random.randint(1,20)
+                if(injuryRoll >= 10):
+                        self.minorInjuries += 1
+                        self.continueFighting = False
+                        self.ableToFight = False
+                        return 0
+                elif(injuryRoll >= 7):
+                        self.moderateInjuries += 1
+                        self.continueFighting = False
+                        self.ableToFight = False
+                        return 0
+                else:
+                        self.majorInjuries += 1
+                        self.continueFighting = False
+                        self.ableToFight = False
+                        return 0
+        def death_roll(self):
+                injuryRoll = random.randint(1,20)
+                if(injuryRoll >= 10):
+                        self.minorInjuries += 1
+                        self.continueFighting = False
+                        self.ableToFight = False
+                        return 0
+                elif(injuryRoll >= 7):
+                        self.moderateInjuries += 1
+                        self.continueFighting = False
+                        self.ableToFight = False
+                        return 0
+                elif(injuryRoll >= 3):
+                        self.majorInjuries += 1
+                        self.continueFighting = False
+                        self.ableToFight = False
+                        return 0
+                else:
+                        self.alive = False
+                        self.continueFighting = False
+                        self.ableToFight = False
+                        return 0
+            
+               
