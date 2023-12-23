d9b1bac0690aeb1574f5b1a99604d5210fcda1a6
IKnowThatIKnowNothin <35470181+IKnowThatIKnowNothin@users.noreply.github.com>
2023-12-23 18:11:10 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -31,49 +31,50 @@ for comment in subreddit.stream.comments(skip_existing=True):
             comments_replied_to.append(comment.id)
     
             if(re.search("Roll",comment.body,re.IGNORECASE)):
-                battleInfo = re.findall("(\d+)([d])(\d+)([\+\-]?\d*)(.*)",comment.body)
-                if(battleInfo):
-                    roundmessage= ""
-                    for j in battleInfo:
-                        bonus = 0
-                        noDice = int(j[0]) #(battleInfo.group(1))
-                        sizeDice = int(j[2]) #(battleInfo.group(3))
-                        if (j[3]):
-                            bonus = int(j[3]) #(battleInfo.group(4))
-                        else:
-                            bonus = 0
-                        name = j[4] #battleInfo.group(5)
-                        number = 0
-                        printedBonus = 0
-                        numberBonus = 0
-                        runningBonus = "("
-            
-                        while(noDice != number):     
-                            random.seed()
-                            printed = random.randint(1,sizeDice)
-                            printedBonus += printed
-                            print("Rolling", name, "\n")
-                            if (noDice - number == 1):
-                                runningBonus += "{})".format(printed)
-                            else:
-                                runningBonus += "{} + ".format(printed)
-                            number += 1
-
-                            
-                        printedBonus += bonus
-                            
-                        if (bonus > 0):
-                            roundmessage += "{}d{}+{} {}: **{}**".format(noDice,sizeDice,bonus,name,printedBonus)
-                            roundmessage += "\n\n {} + {} \n\n *** \n\n".format(runningBonus,bonus)
-                        elif (bonus < 0):
-                            roundmessage += "{}d{}{} {}: **{}**".format(noDice,sizeDice,bonus,name,printedBonus)
-                            roundmessage += "\n\n {} {} \n\n *** \n\n".format(runningBonus,bonus)
-                        elif (noDice > 1):
-                            roundmessage += "{}d{} {}: **{}**".format(noDice,sizeDice,name,printedBonus)
-                            roundmessage += "\n\n {} \n\n *** \n\n".format(runningBonus)
-                        else:
-                            roundmessage += "{}d{} {}: **{}**".format(noDice,sizeDice,name,printedBonus)
-                            roundmessage += "\n\n *** \n\n"
+              try:
+                  battleInfo = re.findall("(\d+)([d])(\d+)([\+\-]?\d*)(.*)",comment.body)
+                  if(battleInfo):
+                      roundmessage= ""
+                      for j in battleInfo:
+                          bonus = 0
+                          noDice = int(j[0]) #(battleInfo.group(1))
+                          sizeDice = int(j[2]) #(battleInfo.group(3))
+                          if (j[3]):
+                              bonus = int(j[3]) #(battleInfo.group(4))
+                          else:
+                              bonus = 0
+                          name = j[4] #battleInfo.group(5)
+                          number = 0
+                          printedBonus = 0
+                          numberBonus = 0
+                          runningBonus = "("
+              
+                          while(noDice != number):     
+                              random.seed()
+                              printed = random.randint(1,sizeDice)
+                              printedBonus += printed
+                              print("Rolling", name, "\n")
+                              if (noDice - number == 1):
+                                  runningBonus += "{})".format(printed)
+                              else:
+                                  runningBonus += "{} + ".format(printed)
+                              number += 1
+  
+                              
+                          printedBonus += bonus
+                              
+                          if (bonus > 0):
+                              roundmessage += "{}d{}+{} {}: **{}**".format(noDice,sizeDice,bonus,name,printedBonus)
+                              roundmessage += "\n\n {} + {} \n\n *** \n\n".format(runningBonus,bonus)
+                          elif (bonus < 0):
+                              roundmessage += "{}d{}{} {}: **{}**".format(noDice,sizeDice,bonus,name,printedBonus)
+                              roundmessage += "\n\n {} {} \n\n *** \n\n".format(runningBonus,bonus)
+                          elif (noDice > 1):
+                              roundmessage += "{}d{} {}: **{}**".format(noDice,sizeDice,name,printedBonus)
+                              roundmessage += "\n\n {} \n\n *** \n\n".format(runningBonus)
+                          else:
+                              roundmessage += "{}d{} {}: **{}**".format(noDice,sizeDice,name,printedBonus)
+                              roundmessage += "\n\n *** \n\n"
 
                     comment.reply(roundmessage)#Post all at once
                     with open("comments_replied_to.txt", "w") as f:
@@ -87,7 +88,10 @@ for comment in subreddit.stream.comments(skip_existing=True):
                     with open("comments_replied_to.txt", "w") as f:
                         for comment_id in comments_replied_to:
                             f.write(comment_id + "\n")
-                time.sleep(60) #We sleep for 3 minutes after each battle so we don't get screwed by rate limits. Delete this when karma is high enough.
+              except:
+                    print ("Improperly formatted roll\n---\n")
+                    comment.reply("Improperly formatted Roll.")
+              time.sleep(60) #We sleep for 3 minutes after each battle so we don't get screwed by rate limits. Delete this when karma is high enough.
                 
                 
             elif(re.search("Naval Battle",comment.body,re.IGNORECASE)):
