82117b2860ca8d65e2f51e40d8e8a5ccca24b871
IKnowThatIKnowNothin <35470181+IKnowThatIKnowNothin@users.noreply.github.com>
2024-01-31 13:38:42 +0000

    Update Dueler.py

--- Dueler.py
+++ Dueler.py
@@ -1,83 +1,32 @@
 import re
 import random
+
 class Dueler:
+
+        commanderName = ""
+        threshold = 0
         name = ""
+        power = 0
         bonus = 0
-        masterwork = False
         continueFighting = True
-        ableToFight = True
-        blockedInjuries = 0
-        minorInjuries = 0
-        moderateInjuries = 0
-        majorInjuries = 0
-        permanentInjuries = "None"
-        alive = True
-        def __init__(self,name,bonus,masterwork):
+        morale = 30
+	
+        def __init__(self, duelerName,threshold,name,power,bonus):
+
+                self.duelerName = duelerName  
+                self.threshold = threshold
                 self.name = name
                 self.bonus = bonus
-                self.masterwork = masterwork
-        def attack_roll(self):
-                return random.randint(1,100) + self.bonus
-        def modify_bonus(self,mod):
-                self.bonus += mod
-        def injury_roll(self):
-                injuryRoll = random.randint(1,20)
-                if(injuryRoll > 15):
-                        self.moderateInjuries += 1
-                        return 5
-                elif(injuryRoll > 5):
-                        self.minorInjuries += 1
-                        return 2
-                else:
-                        return 0
-        def death_roll(self):
-                deathRoll = random.randint(1,20)
-                if(deathRoll > 10):
-                        self.continueFighting = False
-                        self.ableToFight = False
-                        return 0
-                elif(deathRoll > 5):
-                        self.majorInjuries += 1
-                        self.continueFighting = False
-                        self.ableToFight = False
-                        return 15
-                else:
-                        self.continueFighting = False
-                        self.ableToFight = False
-                        permanentRoll = random.randint(1,100)
-                        if (permanentRoll <= 25):
-                                self.alive = False
-                        elif (permanentRoll <= 40):
-                                self.permanentInjuries = "Hand"
-                        elif (permanentRoll <= 55):
-                                self.permanentInjuries = "Foot"
-                        elif (permanentRoll <= 65):
-                                self.permanentInjuries = "Blinded"
-                        elif (permanentRoll <= 75):
-                                self.permanentInjuries = "Defeaned"
-                        elif (permanentRoll <= 40):
-                                self.permanentInjuries = "Internal"
-                        elif (permanentRoll <= 40):
-                                self.permanentInjuries = "Scar"
-                        else:
-                                self.permanentInjuries = "Genital"
-                        print(permanentRoll)
-                        print(self.permanentInjuries)
-                        return 0
-		
-        def broken_roll(self):
-                brokenRoll = random.randint(1,20)
-                if(brokenRoll > 18):
-                        self.majorInjuries += 1
-                        self.continueFighting = False
-                        return 15
-                else:
-                        self.continueFighting = False
-                        self.ableToFight = False
-                        return 0
-		
-        def blunted_roll(self):
-                self.continueFighting = False
-                self.ableToFight = False
-                return 0
 
+        def attack_roll(self):
+		
+                random.seed()
+                return random.randint(1,20) +self.bonus
+
+	def damage_roll(self):
+
+		dmg = 0
+                random.seed()
+		for x in range(2):
+			dmg += random.randint(1,5)
+                return dmg
