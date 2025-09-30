f7cf32b2eb4ebcb29e9edec27bfaa280c9358005
Crazymajor <162837656+Crazymajor@users.noreply.github.com>
2025-09-30 15:18:07 -0400

    Update Jouster.py for Injury Rework
    Updated death_roll to match the rework. Must be paired with changes to joust.py that renames injuries types to match those in the rules.

--- Jouster.py
+++ Jouster.py
@@ -3,13 +3,14 @@ import random
 class Jouster:
         name = ""
         bonus = 0
+        seriousInjuryRoll = 0
         injuryRoll = 0
         continueFighting = True
         ableToFight = True
         brokenLances = 0
         minorInjuries = 0
-        moderateInjuries = 0
         majorInjuries = 0
+        criticalInjuries = 0
         permanentInjuries = "None"
         alive = True
         def __init__(self,name,bonus):
@@ -20,6 +21,10 @@ class Jouster:
                 return random.randint(1,20)
         def modify_bonus(self,mod):
                 self.bonus += mod
+
+       '''
+       This function is not being used.
+       
         def injury_roll(self):
                 random.seed()
                 injuryRoll = random.randint(1,20)
@@ -29,35 +34,41 @@ class Jouster:
                         self.ableToFight = False
                         return 0
                 elif(injuryRoll >= 7):
-                        self.moderateInjuries += 1
+                        self.majorInjuries += 1
                         self.continueFighting = False
                         self.ableToFight = False
                         return 0
                 else:
-                        self.majorInjuries += 1
+                        self.criticalInjuries += 1
                         self.continueFighting = False
                         self.ableToFight = False
                         return 0
+        '''
+
         def death_roll(self):
                 injuryRoll = random.randint(1,20)
-                if(injuryRoll <= 11 and injuryRoll >= 6):
+                if(injuryRoll <= 2 and injuryRoll >= 5):
                         self.minorInjuries += 1
                         self.continueFighting = False
                         self.ableToFight = False
                         return 0
-                elif(injuryRoll <= 5 and injuryRoll >= 3):
-                        self.moderateInjuries += 1
-                        self.continueFighting = False
-                        self.ableToFight = False
-                        return 0
-                elif(injuryRoll == 2):
-                        self.majorInjuries += 1
-                        self.continueFighting = False
-                        self.ableToFight = False
                 elif(injuryRoll == 1):
-                        self.alive = False
-                        self.continueFighting = False
-                        self.ableToFight = False
+                        seriousInjuryRoll = random.randint(1,20)
+                        if(injuryRoll <= 6 and injuryRoll >= 20):
+                                self.majorInjuries += 1
+                                self.continueFighting = False
+                                self.ableToFight = False
+                                return 0
+                        elif(injuryRoll <= 2 and injuryRoll >= 5):
+                                self.criticalInjuries += 1
+                                self.continueFighting = False
+                                self.ableToFight = False
+                                return 0
+                        elif(injuryRoll == 1):
+                                self.alive = False
+                                self.continueFighting = False
+                                self.ableToFight = False
+                                return 0
                 else:
                         self.continueFighting = False
                         self.ableToFight = False
